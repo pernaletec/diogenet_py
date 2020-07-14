614cf1657edc9f2306f2faeded274558f8b4ad3d
César Pernalete <fenixiacp@yandex.com>
2020-07-14 19:29:46 -0400

    First trial with the graph class

--- diogenet_py/network_graph.py
+++ diogenet_py/network_graph.py
@@ -8,13 +8,25 @@ import igraph
 import pandas as pd
 import numpy as np
 from dataclasses import dataclass
-
+#import data_access
 
 @dataclass
-class Graph:
+class map_graph:
     nodes: pd.DataFrame
     edges: pd.DataFrame
     locations: pd.DataFrame
+    
+    def __init__(self, n, e, l, m):
+        self.nodes = n
+        self.edges = e
+        self.locations = l
+        self.map_graph_objm = igraph.Graph()
 
     def know_locations(self):
         return pd.DataFrame()
+
+grafo = map_graph(1,2,3,4)
+
+print(grafo.map_graph_objm)
+
+
