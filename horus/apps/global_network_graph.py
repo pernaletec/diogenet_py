7f1da11ca883a2572fdd7bd58b2138fbdd0992be
Laurex94 <laurencejose94@gmail.com>
2022-01-14 15:10:45 -0400

    1. Reorganized the project structure 2. Created the id of the Local Network Graph sidebar elements

--- horus/apps/global_network_graph.py
+++ horus/apps/global_network_graph.py
@@ -27,44 +27,46 @@ navbar = dbc.Navbar(
 
 sidebar_content = [
     html.H5('Dataset Selection', className="mt-3 mb-3"),
-    dbc.InputGroup(
-            [
-                dbc.Select(
-                    options=[
-                        {"label": "Option 1", "value": 1},
-                        {"label": "Option 2", "value": 2},
-                    ],
-                    placeholder='Select a dataset'
-                ),
-            ]
-     ),
+    dcc.Dropdown(
+        id='edges_select_global',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montreal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        searchable=False,
+        placeholder="Select a dataset"
+    ),
     html.H5('Network Ties', className="mt-5 mb-3"),
-    dcc.Checklist(
-    options=[
-        {'label': ' Is teacher of', 'value': 1},
-            {'label': ' Is friend of', 'value': 2},
-            {'label': ' Is family of', 'value': 3},
-            {'label': ' Studied the work of', 'value': 4},
-            {'label': ' sent letters to', 'value': 5},
-            {'label': ' Is benefactor of', 'value': 6},
-    ],
-    value=[1],
-    labelStyle={'display': 'flex', 'flexDirection':'row','alingItem':'center'},
-    inputStyle={'margin':'0px 5px'},
+    dcc.Checklist( 
+        id='edges_select_global',
+        options=[
+            {'label': ' Is teacher of', 'value': 1},
+                {'label': ' Is friend of', 'value': 2},
+                {'label': ' Is family of', 'value': 3},
+                {'label': ' Studied the work of', 'value': 4},
+                {'label': ' sent letters to', 'value': 5},
+                {'label': ' Is benefactor of', 'value': 6},
+        ],
+        value=[1],
+        labelStyle={'display': 'flex', 'flexDirection':'row','alingItem':'center'},
+        inputStyle={'margin':'0px 5px'},
     ),
     html.H5('Graph Layout',className="mt-5 mb-3"),
-    dbc.InputGroup(
-            [
-                dbc.Select(
-                    options=[
-                        {"label": "Option 1", "value": 1},
-                        {"label": "Option 2", "value": 2},
-                    ],
-                    placeholder='Select a layout'
-                ),
-            ]
-     ),
+    dcc.Dropdown(
+        id='layout_global',
+        options=[
+            {'label': 'Fruchterman-Reingold', 'value': 'layout_with_fr'},
+            {'label': 'Kamada-Kawai', 'value': 'layout_with_kk'},
+            {'label': 'On sphere', 'value': 'layout_on_sphere'},
+            {'label': 'In Circle', 'value': 'layout_in_circle'},
+            {'label': 'On Grid', 'value': 'layout_on_grid'},
+        ],
+        value='layout_with_fr',
+        searchable=False,
+    ),
     dcc.Checklist(
+        id='show_gender',
         options=[
             {'label': ' Gender and Gods', 'value': "true"},
         ],
@@ -75,6 +77,7 @@ sidebar_content = [
     html.H5('Appearence',className="mt-5 mb-3"),
     html.H6('Label Size',className="mt-1 mb-2"),
     dcc.RangeSlider(
+        id='label_size_global',
         min=0,
         max=10,
         step=1,
@@ -91,10 +94,11 @@ sidebar_content = [
             9: '9',
             10: '10',
         },
-        value=[3, 6]
+        value=[4, 6]
     ),
     html.H6('Node Size',className="mt-1 mb-2"),
     dcc.RangeSlider(
+        id='node_size_global',
         min=0,
         max=10,
         step=1,
@@ -111,7 +115,7 @@ sidebar_content = [
             9: '9',
             10: '10',
         },
-        value=[3, 6]
+        value=[4, 6]
     )
 ]
 
