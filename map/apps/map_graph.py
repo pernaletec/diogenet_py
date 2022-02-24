c77cd06e5d8e7d045757708625ca212e29ac1342
Laurex94 <laurencejose94@gmail.com>
2022-02-24 10:08:52 -0400

    added style layout to map_graph

---
+++ map/apps/map_graph.py
@@ -0,0 +1,179 @@
+import dash
+from dash import dcc, html, dash_table
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+import dash_bootstrap_components as dbc
+import pathlib
+import os
+import sys
+import requests
+import tempfile
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from flask import (
+    Flask,
+    render_template,
+    make_response,
+    request,
+    send_from_directory,
+    send_file,
+    jsonify,
+)
+
+from app import app
+from data_analysis_module.network_graph import diogenetGraph
+
+dict_of_datasets = {'Diogenes Laertius': 'diogenes', 'Life of Pythagoras Iamblichus': 'iamblichus'}
+
+STYLE_A_ITEM = {
+    'color':'#ffffff',
+    'textDecoration': 'none'
+}
+
+navbar = dbc.Navbar(
+    children=[
+        html.Div(
+            [
+                dbc.NavLink("Horus", style=STYLE_A_ITEM),
+                dbc.NavLink("Traveler", style=STYLE_A_ITEM),
+            ],
+            className="d-flex",
+
+        ),
+    ],
+    color="#1a6ecc",
+    className="d-flex justify-content-between",
+    style={'color':'#ffffff'},
+    id='navbar-map'
+)
+
+sidebar_content = [
+    html.H5('Dataset selection', className="mt-3 mb-3"),
+    dcc.Dropdown(
+        id='dataset_selection_map',
+        options=[
+            {'label': key, 'value': value}
+            for key, value in dict_of_datasets.items()
+        ],
+        searchable=False,
+        placeholder="Select a dataset",
+        value='diogenes'
+    ),
+    html.H5('traveler',className="mt-5 mb-3"),
+    html.Div(
+        id='dropdown_container_traveler',
+        children=[]
+    ),
+    html.H5('Centrality type',className="mt-5 mb-3"),
+    dcc.Dropdown(
+        id='centrality_type_map',
+        options=[
+            {'label': 'Degree', 'value': 'Degree'},
+            {'label': 'Betweeness', 'value': 'Betweeness'},
+            {'label': 'Closeness', 'value': 'Closeness'},
+            {'label': 'Eigenvector', 'value': 'Eigenvector'},
+        ],
+        value='Degree',
+        searchable=False,
+    ),
+    html.H5('Appearence',className="mt-5 mb-3"),
+    html.H6('Label Size',className="mt-1 mb-2"),
+    dcc.RangeSlider(
+        id='label_size_map',
+        min=0,
+        max=10,
+        step=1,
+        marks={
+            0: '0',
+            1: '1',
+            2: '2',
+            3: '3',
+            4: '4',
+            5: '5',
+            6: '6',
+            7: '7',
+            8: '8',
+            9: '9',
+            10: '10',
+        },
+        value=[4, 6]
+    ),
+    html.H6('Node Size',className="mt-1 mb-2"),
+    dcc.RangeSlider(
+        id='node_size_map',
+        min=0,
+        max=10,
+        step=1,
+        marks={
+            0: '0',
+            1: '1',
+            2: '2',
+            3: '3',
+            4: '4',
+            5: '5',
+            6: '6',
+            7: '7',
+            8: '8',
+            9: '9',
+            10: '10',
+        },
+        value=[4, 6]
+    ),
+    html.H6('Download travel edges graph data',className="mt-5 mb-3"),
+    dbc.Button("Download Data", id="btn_csv_map", color="secondary", className="ml-3"),
+    dcc.Download(id="download-dataframe-csv-map"),
+]
+
+tabs = dcc.Tabs(
+        id='tabs_map', 
+        value='map_graph',
+        parent_className='custom-tabs',
+        className='custom-tabs-container',
+        children=[
+            dcc.Tab(
+                label='Map', 
+                value='map_maps',
+                className='custom-tab',
+                selected_className='custom-tab--selected'    
+            ),
+                
+            dcc.Tab(
+                label='Metrics', 
+                value='map_metrics',
+                className='custom-tab',
+                selected_className='custom-tab--selected'
+            ),
+            dcc.Tab(
+                label='Graph', 
+                value='map_graphs',
+                className='custom-tab',
+                selected_className='custom-tab--selected'
+            ),
+        ])
+
+row = html.Div(
+    [
+        dbc.Row(navbar),
+        dbc.Row(
+            [
+                dbc.Col(html.Div(sidebar_content), id='sidebar_map', width=3, style={"backgroundColor": "#2780e31a", "padding":'30px 10px 10px 10px'}),
+                dbc.Col(html.Div(children=[tabs, html.Div(id="content_map", style={'height': '100vh'}, children=[])]), id='main_map'),
+                dcc.ConfirmDialog(
+                        id='confirm-warning-tie-local-centrality',
+                        message='You must select at least one tie',
+                    ),
+            ],
+            className='h-100'
+        ),
+    ],
+    className='h-100',
+    style={'padding':'0px 10px'}
+)
+
+layout = html.Div(
+    [
+    row
+    ],  
+    style={"height": "100vh"}
+)
--- map/index.py
+++ map/index.py
@@ -6,17 +6,18 @@ import dash_bootstrap_components as dbc
 
 from app import app
 from app import server
+from apps import map_graph
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content', children=[])
+    html.Div(id='page-content-map', children=[])
 ])
 
 # Update the index
-@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+@app.callback(Output('page-content-map', 'children'), [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/':
-        return "home"
+        return map_graph.layout
     else:
         return '404'
 
