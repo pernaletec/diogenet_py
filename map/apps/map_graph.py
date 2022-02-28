1d5fbed3ff8df0acc33e482cfc05fc021d028481
Laurex94 <laurencejose94@gmail.com>
2022-02-28 15:10:08 -0400

    1. Added graph map section 2. Created logic for metrics and map section

--- map/apps/map_graph.py
+++ map/apps/map_graph.py
@@ -182,12 +182,32 @@ layout = html.Div(
     Output('dropdown_container_traveler', 'children'),
     Input('dataset_selection_map', 'value'),)
 def get_traveler(dataset_selection):
-    pass
+    map_graph = diogenetGraph(
+    "map",
+    dataset_selection,
+    dataset_selection,
+    'locations_data.csv',
+    'travels_blacklist.csv'
+    )
+    
+    list_of_traveler = sorted(list(set(map_graph.get_edges_names())))
+
+    return dcc.Dropdown(
+        id='traveler_map',
+        options=[       
+            {'label': traveler, 'value': traveler}
+            for traveler in list_of_traveler
+        ],
+        value="All",
+        searchable=False,
+        multi=True
+    ),
 
 @app.callback(
     Output("content_map", "children"),[
     Input('tabs_map', 'value'),
     Input('dataset_selection_map', 'value'),
+    Input('traveler_map','value'),
     Input('centrality_type_map', 'value'),
     Input('label_size_map', 'value'),
     Input('node_size_map', 'value')
@@ -196,15 +216,119 @@ def get_traveler(dataset_selection):
 def horus_get_local_graph_centrality(
                                     tab,
                                     dataset_selection,
+                                    traveler,
                                     centrality_index,
                                     label_size,  
                                     node_size):
 
+    map_graph = diogenetGraph(
+    "map",
+    dataset_selection,
+    dataset_selection,
+    'locations_data.csv',
+    'travels_blacklist.csv'
+    )    
+
     if tab == "map_maps":
+        map_graph.current_centrality_index = centrality_index
+        if traveler == "All" or traveler == []:
+            map_graph.edges_filter = []
+        if traveler != "All" or traveler != []:
+            for m_filter in traveler:
+                map_graph.set_edges_filter(m_filter)
+
+        map_graph.create_subgraph()
+        data = map_graph.get_map_data(min_weight=node_size[0], max_weight=node_size[1])
+        all_data = map_graph.get_max_min()
+        if data:
+            all_data["data"] = data
+
+        print(all_data)
         return "map"
 
     if tab == "map_metrics":
+
+        def get_map_data_table(filter_string):
+            graph = None
+            graph = map_graph
+            if filter_string != "All" or filter_string != []:
+                for m_filter in filter_string:
+                    graph.set_edges_filter(m_filter)
+                graph.create_subgraph()
+            return (
+                graph.get_vertex_names(),
+                graph.calculate_degree(),
+                graph.calculate_betweenness(),
+                graph.calculate_closeness(),
+                graph.calculate_eigenvector(),
+            )
+
+        
+        if traveler == "All" or traveler == []:
+            traveler = "All"
+
+        graph_type = "map"
+        data = []
+        cities = degree = betweeness = closeness = eigenvector = []
+
+        if graph_type == "map":
+            (cities, degree, betweeness, closeness, eigenvector) = get_map_data_table(
+                traveler
+            )
+
+        for (
+        city_name,
+        city_degree,
+        city_betweeness,
+        city_closeness,
+        city_eigenvector,
+        ) in zip(cities, degree, betweeness, closeness, eigenvector):
+            record = {
+                "City": city_name,
+                "Degree": city_degree,
+                "Betweenness": city_betweeness,
+                "Closeness": city_closeness,
+                "Eigenvector": city_eigenvector,
+            }
+            data.append(record)
+
+        data_table = {}
+        data_table["CentralizationDegree"] = map_graph.centralization_degree()
+        data_table["CentralizationBetweenness"] = map_graph.centralization_betweenness()
+        data_table["CentralizationCloseness"] = map_graph.centralization_closeness()
+        data_table["CentralizationEigenvector"] = map_graph.centralization_eigenvector()
+        data_table["CityData"] = data
+
+        print(data_table)
+
         return  "metrics"
     
-    if tab == "Graph":
-        return  "graph"
+    if tab == "map_graphs":
+
+        map_graph.current_centrality_index = centrality_index
+        
+        graph_layout = "fr"
+        pvis_graph = None
+
+        if traveler == "All" or traveler == []:
+            #map_graph.edges_filter = []
+            pvis_graph = map_graph.get_pyvis(
+                min_weight=node_size[0],
+                max_weight=node_size[1],
+                min_label_size=label_size[0],
+                max_label_size=label_size[1],
+                layout=graph_layout,
+            )
+        else:
+            for m_filter in traveler:
+                map_graph.set_edges_filter(m_filter)
+            map_graph.create_subgraph()
+            pvis_graph = map_graph.get_pyvis()
+
+        if pvis_graph:
+            suffix = ".html"
+            temp_file_name = next(tempfile._get_candidate_names()) + suffix
+            full_filename = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'assets',temp_file_name))
+
+            pvis_graph.write_html(full_filename)
+            return [html.Iframe(src=f"/assets/{temp_file_name}",style={"height":"100%", "width": "100%"})]
